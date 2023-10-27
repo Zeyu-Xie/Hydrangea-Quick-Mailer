e1cc801ddf1cbfb90610dfceeb70d329f9b796bc
Acan <xie.zeyu20@gmail.com>
2023-10-27 23:39:13 +0800

    .

--- index.js
+++ index.js
@@ -1,45 +1,50 @@
 const nodemailer = require("nodemailer")
 const express = require("express")
 const opn = require("opn")
-
-const transporter = nodemailer.createTransport({
-    service: "qq",
-    auth: {
-        user: "your_qq_email@qq.com",
-        pass: "your_qq_email_password"
-    }
-})
-
-const mailOptions = {
-    from: {
-        name: "your_name",
-        address: "your_qq_email@qq.com"
-    },
-    to: "recipient_email@example.com",
-    cc: "cc_email@example.com",
-    bcc: "bcc_email@example.com",
-    subject: "example_subject",
-
-    text: "example_text",
-    html: "<a href=\"https://google.com\">Google</a>"
-}
-
-// transporter.sendMail(mailOptions, (err, info) => {
-//     if(err) {
-//         console.log("ERROR",err)
-//     }
-//     else {
-//         console.log("Successful")
-//     }
-// })
+const path = require("path")
 
 const app = express()
 const port = 3000
 
-opn("https://google.com")
+// 路由部分
 
-app.get("/api", (req,res)=>{
-    res.send("test")
+// 路由到网页
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"))
+})
+// 路由到发送
+app.post("/api/send", (req, res) => {
+
+    const transporter = {
+        service: req.body.transporter.service,
+        auth: {
+            user: req.body.transporter.auth.user,
+            pass: req.body.transporter.auth.pass
+        }
+    }
+    const mailOptions = {
+        from: {
+            name: req.body.mailOptions.from.name,
+            address: req.body.mailOptions.from.address
+        },
+        to: req.body.mailOptions.to,
+        cc: req.body.mailOptions.cc,
+        bcc: req.body.mailOptions.bcc,
+        subject: req.body.mailOptions.subject,
+
+        text: req.body.mailOptions.text,
+        html: req.body.mailOptions.html
+    }
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            res.status(404).send("ERROR " + err)
+            return
+        }
+        else {
+            res.send("Successful")
+            return
+        }
+    })
 })
 
 app.listen(port, () => {
