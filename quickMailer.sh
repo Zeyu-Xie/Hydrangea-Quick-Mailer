ad5cab7efa07e88bb6be491ba602362fbfb07e73
acanxie <xie.zeyu20@gmail.com>
2023-10-28 16:46:10 +0800

    .

--- quickMailer.sh
+++ quickMailer.sh
@@ -1,4 +1,19 @@
-#!/bin/bash
-echo "Starting"
 cd "$(dirname "$0")"
-node server.js
+
+if [ "$1" = "" ]; then
+    echo Starting
+    node server.js
+else
+    if [ "$1" = "install" ]; then
+        if [ "$2" = "" ]; then
+            echo Installing
+            npm install
+        else
+            echo Wrong Parameter
+            exit 1
+        fi
+    else
+        echo Wrong Parameter
+        exit 1
+    fi
+fi
