0239c576424097bc3e4de15e4af484e701520bf4
Acan <xie.zeyu20@gmail.com>
2023-10-28 11:44:16 +0800

    .

--- index.js
+++ index.js
@@ -1,52 +1,11 @@
-const nodemailer = require("nodemailer")
-const express = require("express")
-const opn = require("opn")
-const path = require("path")
+const _serviceProvider = document.getElementById("serviceProvider")
+const _username = document.getElementById("username")
+const _password = document.getElementById("password")
+const _from = document.getElementById("from")
+const _to = document.getElementById("to")
+const _cc = document.getElementById("cc")
+const _bcc = document.getElementById("bcc")
+const _text = document.getElementById("text")
+const _html = document.getElementById("html")
+const _file = document.getElementById("file")
 
-const app = express()
-const port = 3000
-
-// 路由部分
-
-// 路由到网页
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"))
-})
-// 路由到发送
-app.post("/api/send", (req, res) => {
-
-    const transporter = {
-        service: req.body.transporter.service,
-        auth: {
-            user: req.body.transporter.auth.user,
-            pass: req.body.transporter.auth.pass
-        }
-    }
-    const mailOptions = {
-        from: {
-            name: req.body.mailOptions.from.name,
-            address: req.body.mailOptions.from.address
-        },
-        to: req.body.mailOptions.to,
-        cc: req.body.mailOptions.cc,
-        bcc: req.body.mailOptions.bcc,
-        subject: req.body.mailOptions.subject,
-
-        text: req.body.mailOptions.text,
-        html: req.body.mailOptions.html
-    }
-    transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-            res.status(404).send("ERROR " + err)
-            return
-        }
-        else {
-            res.send("Successful")
-            return
-        }
-    })
-})
-
-app.listen(port, () => {
-    console.log("Successful")
-})
---
+++ server.js
@@ -0,0 +1,52 @@
+const nodemailer = require("nodemailer")
+const express = require("express")
+const opn = require("opn")
+const path = require("path")
+
+const app = express()
+app.use(express.static("."))
+const port = 3000
+
+// 路由到网页
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"))
+})
+// 路由到发送
+app.post("/api/send", (req, res) => {
+
+    const transporter = {
+        service: req.body.transporter.service,
+        auth: {
+            user: req.body.transporter.auth.user,
+            pass: req.body.transporter.auth.pass
+        }
+    }
+    const mailOptions = {
+        from: {
+            name: req.body.mailOptions.from.name,
+            address: req.body.mailOptions.from.address
+        },
+        to: req.body.mailOptions.to,
+        cc: req.body.mailOptions.cc,
+        bcc: req.body.mailOptions.bcc,
+        subject: req.body.mailOptions.subject,
+
+        text: req.body.mailOptions.text,
+        html: req.body.mailOptions.html
+    }
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            res.status(404).send("ERROR " + err)
+            return
+        }
+        else {
+            res.send("Successful")
+            return
+        }
+    })
+})
+
+app.listen(port, () => {
+    console.log("Server Created")
+    opn("http://localhost:3000")
+})
